cccd2e6f57edf7f83792bfd28156e6dfaa89bee2
Anatoly <hlystike@rambler.ru>
2022-08-08 20:16:33 +0700

    two pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function () {
 
+    $('.course-review__slider').slick({
+        dots: false,
+        arrows: true,
+        fade: true,
+        autoplay: true,
+        prevArrow: '<button type="button" class="course-review__slider-arrow course-review__slider-arrowleft"><img src="images/arrow-prev.svg"></button>',
+        nextArrow: '<button type="button" class="course-review__slider-arrow course-review__slider-arrowright"><img src="images/arrow-next.svg"></button>'
+    });
 })
