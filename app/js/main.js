ff2902e86c67c3d373d15b5c44134b3aa073a9d0
Anatoly <hlystike@rambler.ru>
2022-08-10 13:04:35 +0700

    all pages

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,10 @@ $(function () {
         prevArrow: '<button type="button" class="course-review__slider-arrow course-review__slider-arrowleft"><img src="images/arrow-prev.svg"></button>',
         nextArrow: '<button type="button" class="course-review__slider-arrow course-review__slider-arrowright"><img src="images/arrow-next.svg"></button>'
     });
+    $('.tariffs__slider').slick({
+        dots: false,
+        arrows: false,
+        fade: true,
+        autoplay: true
+    });
 })
